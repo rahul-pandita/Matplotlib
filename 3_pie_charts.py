8fb36d4e1e70e5e61c1d4faf5780545001010045
Rahul Pandita <rpandita1999@outlook.com>
2024-03-29 13:10:10 +0530

    change colours of slices

--- 3_pie_charts.py
+++ 3_pie_charts.py
@@ -2,10 +2,11 @@ from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
-slices = [60, 40]
-labels = ["Sixty","Forty"]
+slices = [60, 40, 120, 90]
+labels = ["Sixty","Forty", "Extra1", "Extra2"]
+colors = ["blue", "red", "green", "yellow"]
 
-plt.pie(slices, labels=labels, wedgeprops={"edgecolor": "black"})
+plt.pie(slices, labels=labels, colors=colors, wedgeprops={"edgecolor": "black"})
 
 plt.title("Pie Chart")
 plt.tight_layout()
