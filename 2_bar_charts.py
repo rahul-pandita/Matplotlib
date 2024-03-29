72ebc509070958535153edf38461b1d901913bed
Rahul Pandita <rpandita1999@outlook.com>
2024-03-29 12:19:15 +0530

    add bar charts; combine with plot in same figure

---
+++ 2_bar_charts.py
@@ -0,0 +1,34 @@
+from matplotlib import pyplot as plt
+
+plt.style.use("fivethirtyeight")
+
+# ALL DEVS
+# x axis - age ranges
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+
+# y axis - median salaries for ages
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+
+plt.bar(ages_x, dev_y, color="#444444", label="All Devs")
+
+# PYTHON DEVS
+# Median Python Developer Salaries by Age
+py_dev_y = [45372, 48876, 53850, 57287, 63016,
+65998, 70003, 70000, 71496, 75370, 83640]
+
+plt.plot(ages_x, py_dev_y, color='#008fd5', label="Python Devs")
+
+# JS DEVS
+js_dev_y = [37810, 43515, 46823, 49293, 53437,
+ 56373, 62375, 66674, 68745, 68746, 74583]
+plt.plot(ages_x, js_dev_y, color="#e5ae38", label="JavaScript Devs")
+
+plt.legend()
+
+plt.title("Median Salary (USD) by Age")
+plt.xlabel("Ages")
+plt.ylabel("Median Salary (USD)")
+
+plt.tight_layout()
+
+plt.show()
