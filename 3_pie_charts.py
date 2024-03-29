c4efd1b42d2a45595559296e43b8b3d26e88d7d0
Rahul Pandita <rpandita1999@outlook.com>
2024-03-29 13:07:11 +0530

    add labels to slices

--- 3_pie_charts.py
+++ 3_pie_charts.py
@@ -3,8 +3,9 @@ from matplotlib import pyplot as plt
 plt.style.use("fivethirtyeight")
 
 slices = [60, 40]
+labels = ["Sixty","Forty"]
 
-plt.pie(slices)
+plt.pie(slices, labels=labels)
 
 plt.title("Pie Chart")
 plt.tight_layout()
