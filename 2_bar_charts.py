c8231bf7d18cf44c619a849bb6eaf5f12605ba85
Rahul Pandita <rpandita1999@outlook.com>
2024-03-29 12:42:50 +0530

    use Counter from collections module to clean up data in csv file

--- 2_bar_charts.py
+++ 2_bar_charts.py
@@ -1,41 +1,26 @@
+import csv
 import numpy as np
 from matplotlib import pyplot as plt
-
+from collections import Counter
 
 plt.style.use("fivethirtyeight")
 
-# ALL DEVS
-# x axis - age ranges
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
-x_indexes = np.arange(len(ages_x))
-width = 0.25
+with open("data.csv", "r") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
 
-# y axis - median salaries for ages
-dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+    lang_counter = Counter()
 
-plt.bar(x_indexes - width, dev_y, width=width, color="#444444", label="All Devs")
+    for row in csv_reader:
+        lang_counter.update(row["LanguagesWorkedWith"].split(";"))
 
-# PYTHON DEVS
-# Median Python Developer Salaries by Age
-py_dev_y = [45372, 48876, 53850, 57287, 63016,
-65998, 70003, 70000, 71496, 75370, 83640]
+print(lang_counter)
 
-plt.bar(x_indexes, py_dev_y, width=width, color='#008fd5', label="Python Devs")
 
-# JS DEVS
-js_dev_y = [37810, 43515, 46823, 49293, 53437,
- 56373, 62375, 66674, 68745, 68746, 74583]
-plt.bar(x_indexes + width, js_dev_y, width=width, color="#e5ae38", label="JavaScript Devs")
+# plt.title("Median Salary (USD) by Age")
+# plt.xlabel("Ages")
+# plt.ylabel("Median Salary (USD)")
 
-plt.legend()
+# plt.tight_layout()
 
-plt.xticks(ticks=x_indexes, labels=ages_x)
-
-plt.title("Median Salary (USD) by Age")
-plt.xlabel("Ages")
-plt.ylabel("Median Salary (USD)")
-
-plt.tight_layout()
-
-plt.show()
+# plt.show()
 # print(x_indexes)
