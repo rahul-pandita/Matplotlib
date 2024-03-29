87571f63b8769e85fd88fb15e3372f3853b1dc55
Rahul Pandita <rpandita1999@outlook.com>
2024-03-29 13:18:29 +0530

    display percentage of pie chart slices

--- 3_pie_charts.py
+++ 3_pie_charts.py
@@ -7,7 +7,7 @@ slices = [59219, 55466, 47544, 36443, 35917]
 labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
 explode = [0, 0, 0, 0.1, 0]
 
-plt.pie(slices, labels=labels, explode=explode, wedgeprops={"edgecolor": "black"})
+plt.pie(slices, labels=labels, explode=explode, shadow=True, autopct="%1.1f%%",wedgeprops={"edgecolor": "black"})
 
 plt.title("Pie Chart")
 plt.tight_layout()
