b4cea85228c1c466a7be035c1f059ec1047c4cc5
Rahul Pandita <rpandita1999@outlook.com>
2024-03-29 12:20:49 +0530

    bar charts overlap on simply using plt.bar

--- 2_bar_charts.py
+++ 2_bar_charts.py
@@ -16,12 +16,12 @@ plt.bar(ages_x, dev_y, color="#444444", label="All Devs")
 py_dev_y = [45372, 48876, 53850, 57287, 63016,
 65998, 70003, 70000, 71496, 75370, 83640]
 
-plt.plot(ages_x, py_dev_y, color='#008fd5', label="Python Devs")
+plt.bar(ages_x, py_dev_y, color='#008fd5', label="Python Devs")
 
 # JS DEVS
 js_dev_y = [37810, 43515, 46823, 49293, 53437,
  56373, 62375, 66674, 68745, 68746, 74583]
-plt.plot(ages_x, js_dev_y, color="#e5ae38", label="JavaScript Devs")
+plt.bar(ages_x, js_dev_y, color="#e5ae38", label="JavaScript Devs")
 
 plt.legend()
 
