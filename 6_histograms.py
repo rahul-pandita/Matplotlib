c3ac3520e0547a9dd56dd91624c944eae9f2f964
Rahul Pandita <rpandita1999@outlook.com>
2024-03-30 10:19:30 +0530

    use bins

--- 6_histograms.py
+++ 6_histograms.py
@@ -4,8 +4,9 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+bins = [10, 20, 30, 40, 50, 60]
 
-plt.hist(ages, bins=5, edgecolor="black")
+plt.hist(ages, bins=bins, edgecolor="black")
 
 # data = pd.read_csv('data.csv')
 # ids = data['Responder_id']
