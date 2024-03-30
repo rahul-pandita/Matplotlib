a2b857d47725e89eb37db80e5f92f6be5d9ced76
Rahul Pandita <rpandita1999@outlook.com>
2024-03-30 10:20:19 +0530

    remove values from bins

--- 6_histograms.py
+++ 6_histograms.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
-bins = [10, 20, 30, 40, 50, 60]
+bins = [20, 30, 40, 50, 60]
 
 plt.hist(ages, bins=bins, edgecolor="black")
 
