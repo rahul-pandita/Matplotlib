1941101ffa296bb7e6fbbdaad22cebc4fb68255d
Rahul Pandita <rpandita1999@outlook.com>
2024-03-30 08:50:16 +0530

    real world example - number of hours worked by devs each day; use a tuple for legend loc

--- 4_stack_plots.py
+++ 4_stack_plots.py
@@ -2,18 +2,20 @@ from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
+# days instead of minutes
 minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-player1 = [1, 2, 3, 3, 4, 4, 4, 4, 5]
-player2 = [1, 1, 1, 1, 2, 2, 2, 3, 4]
-player3 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
+# devs instead of players
+player1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
+player2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
+player3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
 
 labels = ["player1", "player2", "player3"]
 colors = ["pink", "orange", "green"]
 
 plt.stackplot(minutes, player1, player2, player3, labels=labels, colors=colors)
 
-plt.legend(loc="upper left")
+plt.legend(loc=(0.07, 0.05))
 
 plt.title("Stack Plot")
 plt.tight_layout()
